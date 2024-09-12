261f147afd78610461a0dd0c629c2d2fb8ec276f
bnhao <88442790+bnhao@users.noreply.github.com>
2024-09-12 13:01:19 -0600

    Created a main page

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2/" className="underline text-cyan-600 hover:text-cyan-300">Week 2 Assignments</Link>
+    </main>
+  )
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h2><b>Name:</b> Nguyen Hao Bui</h2>
+            <h3 className="font-bold">Github Link:</h3>
+            <Link href="https://github.com/bnhao/cprg306-assignments" className="underline text-cyan-600 hover:text-cyan-300">https://github.com/bnhao/cprg306-assignments</Link>
+        </main>
+
+    )
+}
