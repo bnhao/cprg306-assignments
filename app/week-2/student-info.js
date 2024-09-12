0bb68397b7e5fae26d0d871334f7f67b29eb45f7
bnhao <88442790+bnhao@users.noreply.github.com>
2024-09-12 13:06:24 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,11 +2,11 @@ import Link from "next/link"
 
 export default function StudentInfo() {
     return (
-        <main>
+        <div>
             <h2><b>Name:</b> Nguyen Hao Bui</h2>
             <h3 className="font-bold">Github Link:</h3>
             <Link href="https://github.com/bnhao/cprg306-assignments" className="underline text-cyan-600 hover:text-cyan-300">https://github.com/bnhao/cprg306-assignments</Link>
-        </main>
+        </div>
 
     )
 }
