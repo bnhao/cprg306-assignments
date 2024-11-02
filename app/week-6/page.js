3b9eea5e668a02f81adc0b93f3e3efef8bd609d5
bnhao <88442790+bnhao@users.noreply.github.com>
2024-11-01 22:57:04 -0600

    update

---
+++ app/week-6/page.js
@@ -0,0 +1,12 @@
+import ItemList from "./item-list";
+import Item from "./item"
+
+export default function Page() {
+
+    return (
+        <main className="min-h-screen bg-gray-900 text-white p-8">
+            <h1 className="text-4xl font-bold mb-6">Shopping List</h1>
+            <ItemList />
+        </main>
+    )
+}
